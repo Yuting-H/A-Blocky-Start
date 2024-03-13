aa7e537a1c00c1bdf4bcedfaa4d7d38676114aac
yhou294 <yhou294@uwo.ca>
2024-03-13 12:52:26 -0400

    refactored LoginView & LoginController

--- A_Blocky_Start/src/mvc/LoginView.java
+++ A_Blocky_Start/src/mvc/LoginView.java
@@ -10,7 +10,7 @@ import javax.swing.JPanel;
  * @author Yuting
  * This is the login page
  */
-public class LoginView extends PanelUI implements Controller{
+public class LoginView {
 	
 	//declaring bounds for components for this panel
 	Rectangle 	usernameLabelBound 		= 	new Rectangle(200, 100, 100, 20);
@@ -18,8 +18,8 @@ public class LoginView extends PanelUI implements Controller{
 	Rectangle 	passwordLabelBound 		= 	new Rectangle(200, 200, 100, 20);
 	Rectangle 	passwordTextFieldBound 	= 	new Rectangle(400, 200, 200, 20);
 	Rectangle 	loginButtonBound 		= 	new Rectangle(350, 300, 100, 20);
-	Point		loginButtonLocation = new Point(350, 300);
-	Dimension 	loginButtonSize = new Dimension(100, 20);
+	Point		loginButtonLocation 	= new Point(350, 300);
+	Dimension 	loginButtonSize 		= new Dimension(100, 20);
 	
 	//declaring components for this panel
 	LabelUI 		usernameFieldLabel 		=	new LabelUI("Username: ", usernameLabelBound);
@@ -28,32 +28,51 @@ public class LoginView extends PanelUI implements Controller{
 	TextFieldUI 	passwordTextField		= 	new TextFieldUI(passwordTextFieldBound);
 	ButtonUI		loginButton 			=  	new ButtonUI(loginButtonLocation, loginButtonSize, "Login");
 	
+	//base panel
+	private static JPanel panel;
 	
 	/**
 	 * 
-	 * @param bound
 	 */
-	LoginView(Dimension bound){
+	public LoginView() {
 		
-		super(bound);
+		panel = new JPanel();
 		
-		loginButton.addActionListener(e -> 
-			FunctionsUI.checkValidLogin(usernameTextField.getText(), passwordTextField.getText()));
-		
-		//adding component 
-		add(usernameFieldLabel);
-		add(usernameTextField);
-		add(passwordFieldLabel);
-		add(passwordTextField);
-		add(loginButton);
-		
+		this.initPanel();
 	}
 
+	/**
+	 * Initialize the login panel
+	 */
+	private void initPanel() {
+		
+		Dimension viewSize = new Dimension(800, 600);
+		
+		panel.setSize(viewSize);
+		panel.setLayout(null);
+
+		
+		panel.setVisible(true);
+		panel.setBackground(Color.gray);
+		panel.add(usernameFieldLabel);
+		panel.add(usernameTextField);
+		panel.add(passwordFieldLabel);
+		panel.add(passwordTextField);
+		panel.add(loginButton);
+		
+	}
+	
+	public void insertPanel () {
+		Main.gameFrame.add(panel);
+	}
+	
+	public void setVisible(boolean visibility) {
+		panel.setVisible(visibility);
+	}
 
 	/**
 	 * Runs when a button switchs the view to this frame
 	 */
-	@Override
 	public void OnEnter() {
 		System.out.println("Entered login screen");
 	}
@@ -62,7 +81,6 @@ public class LoginView extends PanelUI implements Controller{
 	/**
 	 * 
 	 */
-	@Override
 	public void OnExit() {
 	}
 }
