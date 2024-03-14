24ecb35f5f61430e10e6a762f02ac76a5fff5b9f
Simon McCabe <Simon@MacBook-Pro-3.local>
2024-03-14 10:58:16 -0400

    Added 2 new java files.

---
+++ A_Blocky_Start/src/mvc/HighScoreData.java
@@ -0,0 +1,27 @@
+package mvc;
+
+public class HighScoreData {
+	private String usernameList[];
+	private int highScoreList[];
+	
+	public HighScoreData(String fileName) {
+		
+	}
+	
+	public void exportHighScore(String fileName) {
+		
+	}
+	
+	public String getUsername(int index) {
+		return usernameList[index];
+	}
+	
+	public int getHighScore(int index) {
+		return highScoreList[index];
+	}
+	
+	public boolean setNameScore(String username, int highScore) {
+		
+	}
+	
+}
---
+++ A_Blocky_Start/src/mvc/SettingsData.java
@@ -0,0 +1,51 @@
+package mvc;
+
+
+public class SettingsData {
+	
+	private int screenHeight;
+	private int screenWidth;
+	private double volumeLevel;
+	private boolean colourblindMode;
+	
+	public loadSettingsData(String filename) {
+		
+	}
+	
+	public exportSettings(String filename) {
+		
+	}
+	
+	public int getScreenHeight() {
+		return screenHeight;
+	}
+	
+	public int getScreenWidth(){
+		return screenWidth;
+	}
+	
+	public double getVolumeLevel() {
+		return volumeLevel;
+	}
+	
+	public boolean getColourblindMode() {
+		return colourblindMode;
+	}
+	
+	public void setScreenHeight(int screenHeight) {
+		this.screenHeight = screenHeight;
+	}
+	
+	public void setScreenWidth(int screenWidth) {
+		this.screenWidth = screenWidth;
+	}
+	
+	public void setVolumeLevel(double volumeLevel) {
+		this.volumeLevel = volumeLevel;
+	}
+	
+	public void setColourblindMode(int screenHeight) {
+		this.screenHeight = screenHeight;
+	}
+
+}
