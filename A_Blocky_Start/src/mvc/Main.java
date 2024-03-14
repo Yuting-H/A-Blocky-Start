1a5c34cb020ec27d0eccec6858342697e0207953
yhou294 <yhou294@uwo.ca>
2024-03-14 11:26:42 -0400

    added more controller & view classes

--- A_Blocky_Start/src/mvc/Main.java
+++ A_Blocky_Start/src/mvc/Main.java
@@ -25,7 +25,7 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 		
-		configureGameFrame(); 
+		configureGameFrame();
 		//loginController.OnEnter(); //Load First screen
 		
 		mainMenuController.OnEnter();
