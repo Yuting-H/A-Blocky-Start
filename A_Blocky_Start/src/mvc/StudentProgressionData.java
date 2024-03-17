4f779281a7afb3485a1c87058909fd7aa7bada5f
Doyle Blacklock <dblacklo@uwo.ca>
2024-03-17 15:26:13 -0400

    Added file reading to TeacherProgression

--- A_Blocky_Start/src/mvc/StudentProgressionData.java
+++ A_Blocky_Start/src/mvc/StudentProgressionData.java
@@ -1,71 +1,16 @@
 package mvc;
+
 import java.util.ArrayList;
 
 public class StudentProgressionData {
 	private ArrayList<ProgressionData> arrayList;
-	
 
-	StudentProgressionData(String data) {
-		
+	StudentProgressionData() {
+		UserData user = LoginScreen.getActiveUser();
+		arrayList = UserData.getProgressionScreen();
 	}
 
-	public String exportProgressionData() {
-		return null;
-	}
-
-	public int getStageID() {
-		return stageID;
-	}
-
-	public boolean getCompleted() {
-		return completed;
-	}
-
-	public int getShortestSteps() {
-		return shortestSteps;
-	}
-
-	public int getHighestScore() {
-		return highestScore;
-	}
-
-	public int getTimeSpent() {
-		return timeSpent;
-	}
-
-	public int getAttempts() {
-		return attempts;
-	}
-
-	public ActionChainData getActionChain() {
-		return actionChain;
-	}
-
-	public void setStageID(int id) {
-		stageID = id;
-	}
-
-	public void setCompleted(boolean c) {
-		completed = c;
-	}
-
-	public void setShortestSteps(int s) {
-		shortestSteps = s;
-	}
-
-	public void setHighestScore(int h) {
-		highestScore = h;
-	}
-
-	public void setTimeSpent(int t) {
-		timeSpent = t;
-	}
-
-	public void setAttempts(int a) {
-		attempts = a;
-	}
-
-	public void setActionChain(ActionChainData a) {
-		actionChain = a;
+	public ArrayList<ProgressionData> exportProgressionData() {
+		return arrayList;
 	}
 }
--- A_Blocky_Start/src/mvc/TeacherProgressionData.java
+++ A_Blocky_Start/src/mvc/TeacherProgressionData.java
@@ -1,21 +1,31 @@
 package mvc;
+
+import java.io.File;
 import java.util.ArrayList;
 
 public class TeacherProgressionData {
 	private ArrayList<StudentProgressionData> arrayList;
-	
-	TeacherProgressionData()
-	{
+
+	TeacherProgressionData() {
+		ArrayList<String> results = new ArrayList<String>();
+
+		File folder = new File("Path");
+		File[] listOfFiles = folder.listFiles();
+
+		for (int i = 0; i < listOfFiles.length; i++) {
+			if (listOfFiles[i].isFile()) {
+				results.add(listOfFiles[i].getName());
+			}
+		}
+		
 		
 	}
-	
-	public ArrayList<StudentProgressionData> getProgressionData()
-	{
+
+	public ArrayList<StudentProgressionData> getProgressionData() {
 		return arrayList;
 	}
-	
-	public void setProgressionData(ArrayList<StudentProgressionData> arrayList)
-	{
+
+	public void setProgressionData(ArrayList<StudentProgressionData> arrayList) {
 		this.arrayList = arrayList;
 	}
 
