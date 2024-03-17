26191b2a02687a17aeeedbb2f0ccc93b1d2753f2
Chun Ho Chan <ccha232@uwo.ca>
2024-03-17 05:18:01 -0400

    Added the error log controller. Finished implementing ProgressionData.

--- A_Blocky_Start/src/mvc/ProgressionData.java
+++ A_Blocky_Start/src/mvc/ProgressionData.java
@@ -9,6 +9,14 @@ package mvc;
 public class ProgressionData {
 	
 	/**
+	 * Separator between progression data and action chain data
+	 */
+	private static final char separator = '/';
+	/**
+	 * Number of arguments, the action chain count as one argument
+	 */
+	private static final int numArgs = 7;
+	/**
 	 * ID of this stage
 	 */
 	private int stageID;
@@ -38,30 +46,72 @@ public class ProgressionData {
 	private ActionChainData actionChain;
 	
 	/**
-	 * Construct a stage progression entry.
+	 * Construct a stage progression entry. Stage ID must be a unique, non-negative number.
 	 * @param stageID ID of this stage
 	 * @param completed Completion status
 	 */
-	public ProgressionData(int stageID, boolean completed) {
+	public ProgressionData(int stageID) {
 		this.stageID = stageID;
-		this.completed = completed;
+		this.completed = false;
 		this.shortestSteps = 0;
 		this.highestScore = 0;
 		this.timeSpent = 0;
 		this.attempts = 0;
-		// TODO
+		this.actionChain = new ActionChainData();
 	}
 	
 	/**
-	 * Decode the encoded data string and call the constructor. See exportData() for details.
+	 * Decode the encoded data string and call the constructor. Return an empty instance with stage ID = -1 if progression data is corrupted. See exportData() for details.
 	 * @param data Encoded data string
 	 * @return ProgressionData
 	 */
 	public static ProgressionData importData(String data) {
-		String[] dataList = data.split(",");
+		// Find the position of separator character
+		int separatorIndex = data.indexOf(separator);
 		
-		// TODO
-		return new ProgressionData(-1, false);
+		// Split the data into two parts
+		String progressionPart = data.substring(0, separatorIndex);
+		String actionChainPart = data.substring(separatorIndex + 1, data.length());
+		
+		// Remove leading commas before splitting data
+		int i = 0;
+		while ((i < progressionPart.length()) && (progressionPart.charAt(i) == ',')) {
+			++i;
+		}
+		progressionPart.substring(i, progressionPart.length());
+		String[] dataList = progressionPart.split(",");
+		
+		// Decode the data
+		try {
+			if (dataList.length < numArgs) {throw new Exception("Too few arguments for importing ProgressionData.");}
+			if (dataList.length > numArgs) {throw new Exception("Too many arguments for importing ProgressionData.");}
+			int stageID = Integer.parseInt(dataList[0]);
+			boolean completed = Boolean.parseBoolean(dataList[1]);
+			int shortestSteps = Integer.parseInt(dataList[2]);
+			int highestScore = Integer.parseInt(dataList[3]);
+			int timeSpent = Integer.parseInt(dataList[4]);
+			int attempts = Integer.parseInt(dataList[5]);
+			ActionChainData actionChain = ActionChainData.importData(actionChainPart);
+			
+			// Call constructor
+			ProgressionData progression = new ProgressionData(stageID);
+			
+			// Add the data
+			if (completed) {progression.markAsCompleted();}
+			progression.updateShortestSteps(shortestSteps);
+			progression.updateHighestScore(highestScore);
+			progression.addTimeSpent(timeSpent);
+			progression.addAttempts(attempts);
+			progression.setActionChain(actionChain);
+			
+			// Return progression data
+			return progression;
+			
+		} catch (Exception e) {
+			// Return empty progression data
+			Main.errorLogController.appendErrorLog(e);
+			return new ProgressionData(-1);
+		}
 	}
 	
 	/**
@@ -69,17 +119,37 @@ public class ProgressionData {
 	 * The string always begins with "Start" and ends with "End", with each action block separated with ",".<br>
 	 * <br>
 	 * Format: (without whitespace)<br>
-	 * "stageID, completed, shortestSteps, highestScore, timeSpent, attempts, actionChain"<br>
+	 * "stageID, completed, shortestSteps, highestScore, timeSpent, attempts, /, actionChain"<br>
 	 * @return Encoded data string
 	 */
 	public String exportData() {
-		// TODO
-		return "";
+		String result = "";
+		
+		// Append all progression data
+		result += String.valueOf(stageID);
+		result += ',';
+		result += String.valueOf(completed).toUpperCase();
+		result += ',';
+		result += String.valueOf(shortestSteps);
+		result += ',';
+		result += String.valueOf(highestScore);
+		result += ',';
+		result += String.valueOf(timeSpent);
+		result += ',';
+		result += String.valueOf(attempts);
+		result += ',';
+		result += separator;
+		result += ',';
+		
+		// Append action chain
+		result += actionChain.exportData();
+		
+		return result;
 	}
 	
 	/**
 	 * Access the ID of this stage.
-	 * @return
+	 * @return stage ID
 	 */
 	public int getStageID() {
 		return stageID;
@@ -87,7 +157,7 @@ public class ProgressionData {
 
 	/**
 	 * Access the completion status of this stage.
-	 * @return
+	 * @return Completion status
 	 */
 	public boolean getCompleted() {
 		return completed;
@@ -95,7 +165,7 @@ public class ProgressionData {
 
 	/**
 	 * Access the shortest number of steps.
-	 * @return
+	 * @return Shortest number of steps
 	 */
 	public int getShortestSteps() {
 		return shortestSteps;
@@ -103,7 +173,7 @@ public class ProgressionData {
 
 	/**
 	 * Access the highest score.
-	 * @return
+	 * @return Highest score
 	 */
 	public int getHighestScore() {
 		return highestScore;
@@ -111,7 +181,7 @@ public class ProgressionData {
 
 	/**
 	 * Access the time spent (in minutes).
-	 * @return
+	 * @return Time spent
 	 */
 	public int getTimeSpent() {
 		return timeSpent;
@@ -119,7 +189,7 @@ public class ProgressionData {
 
 	/**
 	 * Access the number of attempts.
-	 * @return
+	 * @return Number of attempts
 	 */
 	public int getAttempts() {
 		return attempts;
@@ -127,7 +197,7 @@ public class ProgressionData {
 
 	/**
 	 * Access the stored action chain.
-	 * @return
+	 * @return Action chain
 	 */
 	public ActionChainData getActionChain() {
 		return actionChain;
@@ -180,17 +250,17 @@ public class ProgressionData {
 
 	/**
 	 * Add to the number of attempts.
-	 * @param attempts
+	 * @param attempts Number of attempts
 	 */
 	public void addAttempts(int attempts) {
 		this.attempts += attempts;
 	}
-
+	
 	/**
-	 * Mutate the stored action chain.
-	 * @param actionChain
+	 * Mutate the action chain of this stage.
 	 */
 	public void setActionChain(ActionChainData actionChain) {
 		this.actionChain = actionChain;
 	}
+
 }
