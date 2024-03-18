e6652bd4010800c440a95c22d98bd73b07e91264
Doyle Blacklock <dblacklo@uwo.ca>
2024-03-18 11:11:16 -0400

    Added Student Progression methods

--- A_Blocky_Start/src/mvc/StudentProgressionData.java
+++ A_Blocky_Start/src/mvc/StudentProgressionData.java
@@ -3,14 +3,32 @@ package mvc;
 import java.util.ArrayList;
 
 public class StudentProgressionData {
-	private ArrayList<ProgressionData> arrayList;
+	private ArrayList<ProgressionData> allProgressionList;
+	private int page;
 
 	StudentProgressionData() {
-		UserData user = LoginScreen.getActiveUser();
-		arrayList = UserData.getProgressionScreen();
+		page = 0;
+		allProgressionList = new ArrayList<ProgressionData>();
 	}
 
-	public ArrayList<ProgressionData> exportProgressionData() {
-		return arrayList;
+	public ProgressionData getProgression(int index) {
+		return allProgressionList.get(index + (page - 1) * 10);
+	}
+
+	public void setProgresssion(UserData userData) {
+		allProgressionList = userData.getProgressionList();
+	}
+
+	public int getPage() {
+		return page;
+	}
+
+	public Boolean setPage(int page) {
+		if (page < 1) {
+			return false;
+		}
+
+		this.page = page;
+		return true;
 	}
 }
--- A_Blocky_Start/src/unittest/TeacherProgressionDataTest.java
+++ A_Blocky_Start/src/unittest/TeacherProgressionDataTest.java
@@ -4,9 +4,18 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import mvc.TeacherProgressionData;
+
+import java.util.ArrayList;
+
 class TeacherProgressionDataTest {
 
 	@Test
+	void ConstructEmpty()
+	{
+		
+	}
+	
 	void test() {
 		fail("Not yet implemented");
 	}
