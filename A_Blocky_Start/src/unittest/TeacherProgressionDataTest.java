f9634ec7c2ab26c882638ea836e6a75836b09865
Doyle Blacklock <dblacklo@uwo.ca>
2024-03-21 11:22:01 -0400

    Added test cases

--- A_Blocky_Start/src/unittest/TeacherProgressionDataTest.java
+++ A_Blocky_Start/src/unittest/TeacherProgressionDataTest.java
@@ -5,19 +5,59 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 import mvc.TeacherProgressionData;
+import mvc.UserData;
 
 import java.util.ArrayList;
+
+import javax.xml.crypto.Data;
 
 class TeacherProgressionDataTest {
 
 	@Test
-	void ConstructEmpty()
+	void ConstructEmpty() {
+		assertAll(() -> new TeacherProgressionData());
+	}
+	
+	@Test
+	void getUserDataNegativeIndex()
+	{
+		TeacherProgressionData data = new TeacherProgressionData();
+		assertEquals(null, data.getUserData(-1));
+	}
+	
+	@Test
+	void getUserDataTooLargeIndex()
+	{
+		TeacherProgressionData data = new TeacherProgressionData();
+		assertEquals(null, data.getUserData(100));
+	}
+	
+	@Test
+	void getUserData()
 	{
 		
 	}
 	
-	void test() {
-		fail("Not yet implemented");
+	@Test
+	void getPageCheck()
+	{
+		TeacherProgressionData data = new TeacherProgressionData();
+		assertEquals(1, data.getPage());
+	}
+	
+	@Test
+	void setPageOutOfBounds()
+	{
+		TeacherProgressionData data = new TeacherProgressionData();
+		assertEquals(false, data.setPage(-1));
+	}
+	
+	@Test
+	void checkSetPage()
+	{
+		TeacherProgressionData data = new TeacherProgressionData();
+		data.setPage(2);
+		assertEquals(2, data.getPage());
 	}
 
 }
