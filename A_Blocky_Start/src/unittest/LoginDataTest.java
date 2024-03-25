6004f0f83b62a14bded4368204c113a81c6e7e5a
Eunhak Kim <ekim429@uwo.ca>
2024-03-25 16:23:46 -0400

    Wrote UserDataTest, still needs to fix errors and failing tests; created LoginDataTest

---
+++ A_Blocky_Start/src/unittest/LoginDataTest.java
@@ -0,0 +1,75 @@
+/**
+ * 
+ */
+package unittest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for {@link mvc.LoginData}
+ * @version 0.1
+ * @since Mar 25, 2024
+ * @author Eunhak Kim
+ *
+ */
+class LoginDataTest {
+
+	/**
+	 * Test method for {@link mvc.LoginData#LoginData()}.
+	 */
+	@Test
+	void testLoginData() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link mvc.LoginData#getActiveUserData()}.
+	 */
+	@Test
+	void testGetActiveUserData() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link mvc.LoginData#getMode()}.
+	 */
+	@Test
+	void testGetMode() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link mvc.LoginData#setUsernameInput(java.lang.String)}.
+	 */
+	@Test
+	void testSetUsernameInput() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link mvc.LoginData#setPasswordInput(java.lang.String)}.
+	 */
+	@Test
+	void testSetPasswordInput() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link mvc.LoginData#registerActiveUser()}.
+	 */
+	@Test
+	void testRegisterActiveUser() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link mvc.LoginData#loginActiveUser()}.
+	 */
+	@Test
+	void testLoginActiveUser() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
